6b02c204dd6c7b9161899dc42974a9bc20c14cc1
FUJITA Natsuho <21716079@jwu.ac.jp>
2019-10-07 16:43:11 +0900

    added gcd

--- rational.py
+++ rational.py
@@ -1,10 +1,15 @@
+import math
+
 class Q(object):
     def __init__(self,a,b=1):
-        self.a=a
-        self.b=b
+        gcd=math.gcd(a,b)
+        self.a=a/gcd
+        self.b=b/gcd
+
     def __repr__(self):
         if self.b==1:
             return str(self.a)
+
         return f'{self.a}/{self.b}'
 
     def __add__(self,q):
@@ -14,6 +19,14 @@ class Q(object):
         d=q.b
         return Q(a*d+b*c,b*d)
 
+    def __sub__(self,q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d-b*c,b*d)
+
+
 q1=Q(1,2)
 q2=Q(1,3)
-print(q1+q2)
+print(q1-q2)
