fcb2c09ec88da9a299f426e99b7c9492d3b4de22
FUJITA Natsuho <21716079@jwu.ac.jp>
2019-10-07 16:23:13 +0900

    added +

--- rational.py
+++ rational.py
@@ -5,8 +5,15 @@ class Q(object):
     def __repr__(self):
         if self.b==1:
             return str(self.a)
-        
         return f'{self.a}/{self.b}'
 
-q=Q(1,2)
-print(q)
+    def add(self,q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d+b*c,b*d)
+
+q1=Q(1,2)
+q2=Q(1,3)
+print(q1.add(q2))
