860f4272da182384fb3343c1dae48aef55c5615b
FUJITA Natsuho <21716079@jwu.ac.jp>
2019-10-28 16:50:14 +0900

    output sub1

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-from exp import Val,Add,Mul
+from exp import Val,Add,Mul,Sub,Div
 
 def parse(s:str):
     if s.find('+')>0:
@@ -7,13 +7,27 @@ def parse(s:str):
         s2=s[pos+1:]
         num=Add(parse(s1),parse(s2))
         return num
+    if s.find('-')>0:
+        pos=s.find('-')
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        num=Sub(parse(s1),parse(s2))
+        return num
     if s.find('*')>0:
         pos=s.find('*')
         s1=s[0:pos]
         s2=s[pos+1:]
         num=Mul(parse(s1),parse(s2))
         return num
+    if s.find('/')>0:
+        pos=s.find('/')
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        num=Div(parse(s1),parse(s2))
+        return num
     return Val(int(s))
 
-e=parse("1*2+3")
+a=Sub(1,2)
+print(a.eval())
+e=parse("1-2-3")
 print(e,e.eval())
