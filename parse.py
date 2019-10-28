ee4537acd530a9435f632f77cd9f2360ec24c721
FUJITA Natsuho <21716079@jwu.ac.jp>
2019-10-28 16:33:52 +0900

    output Add,Mul

--- parse.py
+++ parse.py
@@ -1,14 +1,19 @@
-from exp import Val,Add
+from exp import Val,Add,Mul
 
 def parse(s:str):
-    pos=s.find('+')
-    if pos==-1:
-        return Val(int(s))
-    else:
+    if s.find('+')>0:
+        pos=s.find('+')
         s1=s[0:pos]
         s2=s[pos+1:]
         num=Add(parse(s1),parse(s2))
         return num
+    if s.find('*')>0:
+        pos=s.find('*')
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        num=Mul(parse(s1),parse(s2))
+        return num
+    return Val(int(s))
 
-e=parse("1+2+3")
+e=parse("1*2+3")
 print(e,e.eval())
