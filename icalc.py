9ce15e72c2031de3e7047d5ec1859f47d4c8f9b0
FUJITA Natsuho <21716079@jwu.ac.jp>
2019-11-25 16:51:20 +0900

    parser

--- icalc.py
+++ icalc.py
@@ -7,6 +7,65 @@ Value = { [0-9]+ #Int }
 ''')
 parser = pegpy.generate(peg)
 
+class Expr(object):
+    @classmethod
+    def new(cls,v):
+        if isinstance(v,Expr):
+            return v
+        return Val(v)
+
+class Val(Expr):
+    __slot__=['value']
+
+    def __init__(self,value):
+        self.value=Value
+
+    def __repr__(self):
+        return f'Val({self.value})'
+
+    def eval(self,env:dict):
+        return self.value
+
+e = Val(0)
+assert e.eval({})==0
+
+class Binary(Expr):
+    __slot__=['left','right']
+
+    def __init__(self,left,right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+
+class Add(Binary):
+    __slot__ = ['left','right']
+
+    def eval(self,env:dict):
+        return self.left.eval(env)+self.right.eval(env)
+
+class Sub(Binary):
+    __slot__ = ['left','right']
+
+    def eval(self,env:dict):
+        return self.left.eval(env)-self.right.eval(env)
+
+class Mul(Binary):
+    __slot__ = ['left','right']
+
+    def eval(self,env:dict):
+        return self.left.eval(env)*self.right.eval(env)
+
+class Div(Binary):
+    __slot__ = ['left','right']
+
+    def eval(self,env:dict):
+        return self.left.eval(env)//self.right.eval(env)
+
+
+
 def calc(t):
     if t == 'Int':
         return int(str(t))
@@ -22,8 +81,10 @@ print(repr(t))
 print(calc(t))
 
 
+
+
 def main():
-    s = input('$ ')
+    s = input('>>>')
     t = parser(s)
     print(calc(t))
 
